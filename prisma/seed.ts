91d9cdffd9ed42ecd320b40fcecaf53a00b42d5b
abdurrozaq63 <rozaqmetalhead06@gmail.com>
2026-05-22 13:06:44 +0700

    update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,34 +1,36 @@
-import { PrismaNeon } from "@prisma/adapter-neon";
-import { PrismaClient, RoleName } from "../app/generated/prisma/client";
-import { Pool, neonConfig} from "@neondatabase/serverless"
-import ws from "ws"
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { PrismaClient, RoleName } from '@prisma/client';
+import { Pool, neonConfig } from '@neondatabase/serverless';
+import ws from 'ws';
 
-neonConfig.webSocketConstructor = ws
+neonConfig.webSocketConstructor = ws;
 
 // const pool = new Pool({
 //     connectionString: process.env.DIRECT_URL!,
 // })
 
 const adapter = new PrismaNeon({
-    connectionString: process.env.DIRECT_URL!,
-})
-const prisma = new PrismaClient({adapter})
+  connectionString: process.env.DIRECT_URL!,
+});
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
-    await prisma.role.createMany({
-        data: [
-            {name: RoleName.OWNER},
-            {name: RoleName.ADMIN},
-            {name: RoleName.MEMBER},
-        ],
-        skipDuplicates: true,
-    })
+  await prisma.role.createMany({
+    data: [
+      { name: RoleName.OWNER },
+      { name: RoleName.ADMIN },
+      { name: RoleName.MEMBER },
+    ],
+    skipDuplicates: true,
+  });
 }
 
-main().catch((e) => {
+main()
+  .catch((e) => {
     console.error(e);
-    process.exit(1)
-}).finally(async () => {
+    process.exit(1);
+  })
+  .finally(async () => {
     await prisma.$disconnect();
-   // await pool.end
-})
+    // await pool.end
+  });
