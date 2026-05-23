8b4d65ba19e0ad684af4e7a3675dd8690191f4af
abdurrozaq63 <rozaqmetalhead06@gmail.com>
2026-05-23 14:28:07 +0700

    add debugging page.tsx landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image';
+import { auth } from '@/lib/auth';
 
 export default async function Home() {
+  const session = await auth();
+  console.log('session page.tsx', session);
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -31,6 +35,7 @@ export default async function Home() {
             </a>{' '}
             center.
           </p>
+          <p>Session {session?.user.id}</p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
